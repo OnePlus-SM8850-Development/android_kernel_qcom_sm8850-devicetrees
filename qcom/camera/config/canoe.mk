37e06fae0aa2e315a8c5a0a92b9552a890b1f48f
chandu078 <chandudyavanapelli03@gmail.com>
2026-08-23 09:28:11 +0000

    qcom: camera: Import oplus changes from CPH2745_16.0.9.400(EX01)
    Change-Id: I61544632c4d1fe79f92fd377ada7ee93bf9ee7b6

--- qcom/camera/config/canoe.mk
+++ qcom/camera/config/canoe.mk
@@ -1,22 +1,24 @@
 dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera.dtbo \
-	alor-interposer-camera.dtbo \
-	alor-interposer-camera-sensor-cdp.dtbo \
-	alor-interposer-camera-sensor-mtp.dtbo \
-	alor-interposer-camera-sensor-qrd.dtbo \
+#	alor-interposer-camera.dtbo \
+#	alor-interposer-camera-sensor-cdp.dtbo \
+#	alor-interposer-camera-sensor-mtp.dtbo \
+#	alor-interposer-camera-sensor-qrd.dtbo \
 	alor-camera.dtbo \
-	whale-camera.dtbo
-dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-v2.dtbo \
-	alor-interposer-camera-v2.dtbo \
-	canoe-camera-sensor-mtp.dtbo \
-	canoe-camera-sensor-cdp.dtbo \
-	canoe-camera-sensor-qrd.dtbo \
-	canoe-camera-sensor-hdk.dtbo \
-	canoe-camera-sensor-rumi.dtbo \
-	alor-camera-sensor-mtp.dtbo \
 	alor-camera-sensor-cdp.dtbo \
-	alor-camera-sensor-qrd.dtbo \
-	alor-camera-sensor-mtp-peach.dtbo \
-	alor-camera-sensor-rcm-peach.dtbo
+	alor-camera-sensor-qrd.dtbo
+#	whale-camera.dtbo
+dtbo-$(CONFIG_ARCH_CANOE)   += canoe-camera-v2.dtbo \
+	alor-interposer-camera-v2.dtbo
+#	canoe-camera-sensor-mtp.dtbo \
+#	canoe-camera-sensor-cdp.dtbo \
+#	canoe-camera-sensor-qrd.dtbo \
+#	canoe-camera-sensor-hdk.dtbo \
+#	canoe-camera-sensor-rumi.dtbo \
+#	alor-camera-sensor-mtp.dtbo \
+#	alor-camera-sensor-cdp.dtbo \
+#	alor-camera-sensor-qrd.dtbo \
+#	alor-camera-sensor-mtp-peach.dtbo \
+#	alor-camera-sensor-rcm-peach.dtbo
 dtbo-$(CONFIG_ARCH_CHORA) += chora-camera.dtbo \
 	chora-camera-sensor-mtp.dtbo \
 	chora-camera-sensor-qrd.dtbo \
@@ -27,3 +29,6 @@ dtbo-$(CONFIG_ARCH_BOURTZI) += bourtzi-camera.dtbo \
 	bourtzi-camera-sensor-qrd.dtbo \
 	bourtzi-camera-sensor-cdp.dtbo \
 	bourtzi-camera-sensor-rcm.dtbo
+
+dtbo-$(CONFIG_ARCH_CANOE)   += oplus/infiniti-camera-sensor-mtp-preT0.dtbo       \
+                               oplus/infiniti-camera-sensor-mtp-T0.dtbo
